74b8f4534eb76f243d13f8f041bda3c542f061f2
Chris Kimball <ckimball62@gmail.com>
2019-12-05 12:36:50 -0500

    added .gitignore

--- app/server.js
+++ app/server.js
@@ -3,11 +3,15 @@ var router = require('../routes/issues');
 var users = require('../routes/users');
 var body_parser = require('body-parser');
 var app = express();
-var SERVER_PORT = 3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(body_parser.json());
 app.use('/', router);
 app.use('/users', users);
-app.listen(3000, () => { console.log(`You're Now listening on port ${SERVER_PORT}`)});
+app.listen(port, () => { console.log(`You're Now listening on port ${port}`)});
 module.exports = {app};
